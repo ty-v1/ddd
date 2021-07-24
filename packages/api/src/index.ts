a8897e063238b31d85ecba21828a3b0b5e84a38d
ty-v1 <fmtyd509@gmail.com>
2021-07-24 18:00:19 +0900

    define project api

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,1 +1,4 @@
-export const a = 10;
+import * as ProjectRequest from './request/project';
+import * as ProjectResponse from './response/project';
+
+export { ProjectRequest, ProjectResponse };
---
+++ packages/api/src/request/project.ts
@@ -0,0 +1,9 @@
+export type CreateProjectRequest = {
+  readonly name: string;
+  readonly description: string;
+};
+
+export type UpdateProjectRequest = {
+  readonly name: string;
+  readonly description: string;
+};
