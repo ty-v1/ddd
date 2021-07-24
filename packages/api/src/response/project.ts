89eb566e80d7c56910158154a62f2f9d340fd415
ty-v1 <fmtyd509@gmail.com>
2021-07-24 18:00:19 +0900

    define project api

---
+++ packages/api/src/response/project.ts
@@ -0,0 +1,7 @@
+export type ProjectResponse = {
+  readonly id: string;
+  readonly name: string;
+  readonly description: string;
+  readonly createDateTime: string;
+  readonly updateDateTime: string;
+};
