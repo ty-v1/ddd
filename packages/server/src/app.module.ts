e73da2567ef801ab38e866bc0cda0fe21b7175b7
ty-v1 <fmtyd509@gmail.com>
2021-07-24 17:07:14 +0900

    enable hot reload

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { RepositoryModule } from '@/repository/RepositoryModule';
 import { ProjectModule } from '@/project/ProjectModule';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RepositoryModule, ProjectModule],
+  imports: [ConfigModule.forRoot(), RepositoryModule, ProjectModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,10 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { PrismaService } from '@/repository/PrismaService';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 
   const prismaService: PrismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
