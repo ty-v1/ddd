80714961e39c7b5df5d883230a59e3262d8b54f2
ty-v1 <fmtyd509@gmail.com>
2021-07-24 18:22:23 +0900

    register usecases to module

--- packages/server/src/project/ProjectModule.ts
+++ packages/server/src/project/ProjectModule.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { DetectProjectDuplicationDomainService } from '@/project/model/service/DetectProjectDuplicationDomainService';
 import { CreateProjectUseCase } from '@/project/usecase/CreateProjectUseCase';
 import { ProjectController } from '@/project/controller/ProjectController';
+import { GetProjectUseCase } from '@/project/usecase/GetProjectUseCase';
+import { UpdateProjectUseCase } from '@/project/usecase/UpdateProjectUseCase';
+import { DeleteProjectUseCase } from '@/project/usecase/DeleteProjectUseCase';
 
 @Module({
   controllers: [ProjectController],
-  providers: [DetectProjectDuplicationDomainService, CreateProjectUseCase],
+  providers: [
+    DetectProjectDuplicationDomainService,
+    CreateProjectUseCase,
+    GetProjectUseCase,
+    UpdateProjectUseCase,
+    DeleteProjectUseCase,
+  ],
 })
 export class ProjectModule {}
