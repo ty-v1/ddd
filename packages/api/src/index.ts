b99619751005659b4b98471eb8e4126d131ac86a
ty-v1 <fmtyd509@gmail.com>
2021-08-14 22:11:33 +0900

    add projectId

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,4 +1,6 @@
 import * as ProjectRequest from './request/project';
 import * as ProjectResponse from './response/project';
+import * as LabelRequest from './request/label';
+import * as LabelResponse from './response/label';
 
-export { ProjectRequest, ProjectResponse };
+export { ProjectRequest, ProjectResponse, LabelRequest, LabelResponse };
