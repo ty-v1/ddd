c5532cdb1409c9f1fcae2f4bb15cf5fdd29f7b2f
ty-v1 <fmtyd509@gmail.com>
2021-08-14 22:11:33 +0900

    add projectId

---
+++ packages/api/src/response/label.ts
@@ -0,0 +1,8 @@
+export type LabelResponse = {
+  readonly id: string;
+  readonly name: string;
+  readonly description: string;
+  readonly color: string;
+  readonly createDateTime: string;
+  readonly updateDateTime: string;
+};
