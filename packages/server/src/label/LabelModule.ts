b1f67b2a963c772adc13223e115ddd3d752ffa98
ty-v1 <fmtyd509@gmail.com>
2021-08-14 22:12:08 +0900

    add controller to module

--- packages/server/src/label/LabelModule.ts
+++ packages/server/src/label/LabelModule.ts
@@ -3,9 +3,10 @@ import { CreateLabelUseCase } from '@/label/model/usecase/CreateLabelUseCase';
 import { UpdateLabelUseCase } from '@/label/model/usecase/UpdateLabelUseCase';
 import { DeleteLabelUseCase } from '@/label/model/usecase/DeleteLabelUseCase';
 import { DetectLabelDuplicationDomainService } from '@/label/model/service/DetectLabelDuplicationDomainService';
+import { LabelController } from '@/label/controller/LabelController';
 
 @Module({
-  controllers: [],
+  controllers: [LabelController],
   providers: [CreateLabelUseCase, UpdateLabelUseCase, DeleteLabelUseCase, DetectLabelDuplicationDomainService],
   exports: [],
 })
