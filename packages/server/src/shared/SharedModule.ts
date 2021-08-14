ac8276e4884e60f064de54e6438d5a8592f373f8
ty-v1 <fmtyd509@gmail.com>
2021-08-14 21:41:09 +0900

    add LabelModule

--- packages/server/src/shared/SharedModule.ts
+++ packages/server/src/shared/SharedModule.ts
@@ -2,11 +2,9 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaService } from '@/repository/PrismaService';
 import { DETECT_PROJECT_EXISTENCE_DOMAIN_SERVICE } from '@/shared/project/DetectProjectExistenceDomainService';
 import { DetectProjectExistenceDomainServiceImpl } from '@/project/model/service/DetectProjectExistenceDomainServiceImpl';
-import { ProjectModule } from '@/project/ProjectModule';
 
 @Global()
 @Module({
-  imports: [ProjectModule],
   providers: [
     PrismaService,
     {
