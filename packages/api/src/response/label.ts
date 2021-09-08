2c9615a7a83db1a1fda2cbdfc803b02196d22f97
ty-v1 <tomidayuya@team-lab.com>
2021-09-08 18:09:40 +0900

    use open-api-generator to generate classes from swagger

--- packages/api/src/response/label.ts
+++
@@ -1,8 +0,0 @@
-export type LabelResponse = {
-  readonly id: string;
-  readonly name: string;
-  readonly description: string;
-  readonly color: string;
-  readonly createDateTime: string;
-  readonly updateDateTime: string;
-};
