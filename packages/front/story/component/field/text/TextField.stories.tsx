1160279eb1c7b8eaf7fca25865d81ad5b0c405db
ty-v1 <fmtyd509@gmail.com>
2021-10-04 17:15:47 +0900

    add input components

---
+++ packages/front/story/component/field/text/TextField.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import TextField from '@/component/field/text/TextField';
+
+TextField.displayName = 'Text';
+
+export default {
+  component: TextField,
+  title: 'TextField',
+} as Meta;
+
+type Props = React.ComponentProps<typeof TextField>;
+
+const Template: Story<Props> = (args) => (
+  <TextField {...args}/>
+);
+
+export const Text = Template.bind({});
+Text.args = {
+  type: 'text',
+  value: 'text',
+};
+
+export const Number = Template.bind({});
+Number.args = {
+  type: 'number',
+  value: 10,
+};
