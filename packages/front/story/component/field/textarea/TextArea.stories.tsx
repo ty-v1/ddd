42cbd8ae9558ed762702268c50f4964817174982
ty-v1 <tomidayuya@team-lab.com>
2021-10-04 17:15:47 +0900

    add input components

---
+++ packages/front/story/component/field/textarea/TextArea.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+import TextArea from '@/component/field/textarea/TextArea';
+
+TextArea.displayName = 'Text';
+
+export default {
+  component: TextArea,
+  title: 'TextArea',
+} as Meta;
+
+const Template = () => (
+  <TextArea/>
+);
+
+export const Default = Template.bind({});
