a100a050d48b2f36bdb56877f2ef2aa36556b9b5
ty-v1 <fmtyd509@gmail.com>
2021-12-10 17:59:49 +0900

    add template

---
+++ packages/front/src/page/PageTemplate.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const PageTemplate: React.FC = ({ children }) => {
+  return (
+    <div>
+      <main>
+        <nav>
+          {/*TODO パンくず*/}
+        </nav>
+        <div>
+          {{ children }}
+        </div>
+      </main>
+    </div>
+  );
+};
